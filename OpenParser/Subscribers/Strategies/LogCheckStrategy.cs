70915e95946bd57f627bb4a994f44be65593f236
Daniel Dorantes <daniel@thesmallbang.com>
2016-11-15 06:10:43 -0600

    Added LogcheckStrategy

---
+++ OpenParser/Subscribers/Strategies/LogCheckStrategy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OpenParser.Subscribers.Strategies
+{
+    public class LogCheckStrategy<T> : ISubscriberStrategy<T>
+    {
+        public LogCheckStrategy(Func<LogEntry, bool> check, Func<LogEntry, T> resultHandler)
+        {
+            Check = check;
+            ResultHandler = resultHandler;
+        }
+
+
+        private Func<LogEntry, T> ResultHandler { get; }
+        private Func<LogEntry, bool> Check { get; }
+
+        public bool IsMatch(LogEntry entry)
+        {
+            return Check(entry);
+        }
+
+        public T GetResult(LogEntry entry)
+        {
+            return ResultHandler(entry);
+        }
+    }
+}
