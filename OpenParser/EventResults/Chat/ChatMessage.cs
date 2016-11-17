f145a695bdfe07dc8999d159cb400a505206c00d
Daniel Dorantes <daniel@thesmallbang.com>
2016-11-17 02:09:58 -0600

    Fixed LogEntry existing in ChatMessage and EventResult

--- OpenParser/EventResults/Chat/ChatMessage.cs
+++ OpenParser/EventResults/Chat/ChatMessage.cs
@@ -4,14 +4,11 @@
     {
         public ChatMessage(LogEntry entry, string from, string message) : base(entry)
         {
-            LogEntry = entry;
             From = from;
             Message = message;
         }
 
         public string From { get; private set; }
         public string Message { get; private set; }
-
-        public LogEntry LogEntry { get; private set; }
     }
 }
